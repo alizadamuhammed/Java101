d5b4a0a0196950a4c2d11395d0a6d2df8ef517b4
Muhammed Alizada <119886075+alizadamuhammmed@users.noreply.github.com>
2025-04-03 13:24:39 +0400

    feat: PowerPrinter program added

---
+++ src/main/java101/loops/PowerPrinter.java
@@ -0,0 +1,20 @@
+package main.java101.loops;
+
+import java.util.Scanner;
+
+public class PowerPrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+
+        for(int i = 1; i <= number; i*=4){
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        for(int i = 1; i <= number; i*=5){
+            System.out.print(i + ", ");
+        }
+    }
+}
